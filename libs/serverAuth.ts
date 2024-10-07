06fc05104e3bb44487fd2ecf3db6bef01dcd70a3
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-10-07 15:41:20 +0300

    Add serverAuth.ts  file

---
+++ libs/serverAuth.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+
+const serverAuth = async(req: NextApiRequest){
+    const session = getSession({ req });
+
+    if (!session?.user?.email){
+        throw new Error("Session or user or email not found. serverAuth.ts");
+    }
+
+    const currentUser = await prisma?.user.findUnique({
+        where:{
+            email: session.user.email,
+        },
+    });
+
+    if (!currentUser){
+        throw new Error("CurrentUser not found in prismadb. serverAuth.ts");
+    }
+
+    return {currentUser};
+};
+
+export default serverAuth;
