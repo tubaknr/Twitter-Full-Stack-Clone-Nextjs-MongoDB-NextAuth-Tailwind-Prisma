1ef71549df7db2d1e114ce6233205b18ef133658
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-10-07 15:57:17 +0300

    Add useCurrentUser.ts  file

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,1 +1,10 @@
 import useSWR from "swr";
+import fetcher from "@/libs/fetcher";
+
+//data ya bakacak, ihtiyaç duyarsa tekrar çekecek.
+const useCurrentUser = () => {
+    const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
+    return { data, error, isLoading, mutate };
+}
+
+export default useCurrentUser;
