160db0ee80183aed8647919ec24b229c68da1d09
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-10-07 21:16:15 +0300

    Add followers section with Avatars

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,3 +1,8 @@
+import useUser from "@/hooks/useUser";
+import { useCallback } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
 interface AvatarProps{
     userId: string;
     isLarge?: boolean;
@@ -5,9 +10,46 @@ interface AvatarProps{
 }
 
 const Avatar:React.FC<AvatarProps> = ({userId, isLarge, hasBorder}) => {
+    
+    const router = useRouter();
+    const { data: fetchedUser } = useUser(userId);
+    
+    const onClick = useCallback((event: any) => {
+        event.stopPropagation(); //avatara tıklandığında, avatarın içinde bulunduğu ve tıklanabilir olan elementlerin tıklanabilirliği iptal olsun ve sadece avatara tıklanmış gibi olsun.
+        
+        //avatar a tıklandığında o user a gitsin.
+        const url = `/users/${userId}`;
+        router.push(url);
+    
+    }, [router, userId]);
+    
     return(
-        <div>
+        <div className={`
+
+            ${hasBorder ? "border-4" : "border-black"}
+            ${isLarge ? "h-32 w-32" : "h-12 w-12"}
+         
+            rounded-full
+            hover:opacity-90
+            transition
+            cursor-pointer
+            relative
+
+            `}>
+            
+            <Image
+                fill 
+                style={{
+                    objectFit: "cover",
+                    borderRadius: "100%"
+                }}
+                alt="Avatar"
+                onClick={onClick}
+                src={fetchedUser?.profileImage || '/images/placeholder.png'}
+            />
 
         </div>
     )
 }
+
+export default Avatar;
--- components/FollowBar.tsx
+++ components/FollowBar.tsx
@@ -1,10 +1,14 @@
 import useUsers from "@/hooks/useUsers";
+import Avatar from "./Avatar";
 
 const FollowBar = () => {
 
     const { data: users = [] } = useUsers();
     
     
+    if (!users || users.length === 0){
+        return null;
+    }
     
     
     return(
@@ -14,7 +18,23 @@ const FollowBar = () => {
                     Who to follow?
                 </h2>
                 <div className="flex flex-col gap-6 mt-4">
-                    {/* TODO USER LIST */}
+                    
+                    {users.map((user: Record<string, any>) => (
+                        
+                        <div key={user.id}
+                             className="flex flex-row gap-4">
+                            <Avatar userId={user.id}/>
+                            <div className="flex flex-col">
+                                <p className="text-white font-semibold text-sm">
+                                    {user.name}
+                                </p>
+                                <p className="text-neutral-400 text-sm">
+                                    @{user.username}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+
                 </div>
 
             </div>
