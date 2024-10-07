374ffa4d44c069acbda6d83731e8322d4005e3c7
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-10-07 15:30:20 +0300

    Add register.ts  file

---
+++ pages/api/register.ts
@@ -0,0 +1,31 @@
+import prisma from "@/libs/prismadb";
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from 'bcrypt';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    if (req.method !== "POST"){
+        return res.status(405).end();
+    }
+    try{
+        const [email, username, name, password] = req.body();
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await prisma.user.create({
+            data: {
+                email, username, name, hashedPassword 
+            }
+        });
+        
+        return res.status(200).json(user);
+
+    }catch(error){
+        console.log("Try error. Register.ts");
+    }
+
+    else{
+        console.log("Method is not POST. Register.ts")
+        return res.status(400).end();
+    }
+}
