86775e2315cc6f0ab930a0e4692e69e9b82571aa
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-10-07 15:20:07 +0300

    Add [...netauth].ts file

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,54 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import prisma from "@/libs/prismadb";
+import NextAuth from "next-auth";
+import bcrypt from 'bcrypt';
+
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: 'credentals',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' },
+            },
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password){
+                    throw new Error(" Email and password required. ...Nextauth");
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if(!user || !user?.hashedPassword){
+                    throw new Error("No user found. Invalid credentials. ...Nextauth");
+                }
+
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
+
+                if(!isCorrectPassword){
+                    throw new Error("Invalid credentials. Passwords dont match. ...Nextauth");
+                }
+
+                return user;
+            } 
+
+        })
+    ],
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET
+});
