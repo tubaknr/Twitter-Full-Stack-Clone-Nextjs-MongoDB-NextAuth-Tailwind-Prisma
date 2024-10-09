59b7ae7ca318ffeba4d3b0efe9aebf120d35ddc1
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-10-09 14:06:40 +0300

    Fix all bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { Metadata } from "next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import FollowBar from "@/components/FollowBar";
@@ -27,53 +28,65 @@ interface LayoutContentProps{
   children: React.ReactNode;
 }
 
-
 const LayoutContent:React.FC<LayoutContentProps> = ({ children }) => {
-  const { data: sessionData } = useSession();
-  const loginModal = useLoginModal();
-  const registerModal = useRegisterModal();
 
-  return(
-  <>
+    const { data: sessionData, status } = useSession();
+
+    console.log("sessionData:",sessionData);
+
+    const loginModal = useLoginModal();
+    const registerModal = useRegisterModal();
+
+    if(status == "loading"){
+      console.log("LOADING LAYOUT.TSX");
+    }
+
+    return(
+    <>
+  
         <Toaster />
       
-      {!sessionData && registerModal.isOpen && <RegisterModal />}
-      {!sessionData && loginModal.isOpen && <LoginModal />}
-      
-       <div className="h-screen bg-black">
-        <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-          <div className="grid grid-cols-4 h-full">
-            
-            <Sidebar/>
-            
-            <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
-                {children}
+          {status === "unauthenticated" && registerModal.isOpen && <RegisterModal />}
+          {status === "unauthenticated" && loginModal.isOpen && <LoginModal />}
+          
+          {(
+          <div className="h-screen bg-black">
+            <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+              <div className="grid grid-cols-4 h-full">
+                
+                <Sidebar/>
+                
+                <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+                    {children}
+                </div>
+
+                <FollowBar />
+
+              </div>
             </div>
-
-            <FollowBar />
-
           </div>
-        </div>
-       </div>
-      
-       </>
-    
+          )}
+        
+         
+      </>
+  
   );
 }    
 
-
-const Layout: React.FC<LayoutProps> = ({ children, session }) => {
-
+const Layout: React.FC<LayoutProps> = ({children, session}) => {
   return(
     <html lang="en">
       <body>
       
         <SessionProvider session={session}>
-          <LayoutContent children={children}/>
+          <LayoutContent>{children}</LayoutContent>
         </SessionProvider>
-            
-      </body>
-    </html>
+          
+          </body>
+        </html>
   )
-}    
+
+}
+
+
 export default Layout;
